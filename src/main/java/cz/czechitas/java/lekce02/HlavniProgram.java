108c8c4d894a4deb240b13702a2d150f0af93623
Lenka Erbenova <lenka.erbenova79@gmail.com>
2025-04-08 20:32:32 +0200

    Zofka is drawing three houses.

--- src/main/java/cz/czechitas/java/lekce02/HlavniProgram.java
+++ src/main/java/cz/czechitas/java/lekce02/HlavniProgram.java
@@ -11,8 +11,28 @@ public class HlavniProgram {
 
   public void start() {
     zofka = new Turtle();
+    for (int i = 0; i < 3; i++) {
+      drawHouse();
+      zofka.turnRight(45);
+      zofka.move(100);
+      zofka.turnLeft(90);
+      zofka.penUp();
+      zofka.move(20);
+      zofka.penDown();
+      zofka.turnLeft(90);
+    }
+  }
 
-    //TODO tady bude kód
+  public void drawHouse() {
+    for (int i = 0; i < 4; i++) {
+      zofka.move(100);
+      zofka.turnRight(90);
+    }
+    zofka.move(100);
+    zofka.turnRight(45);
+    zofka.move(70);
+    zofka.turnRight(90);
+    zofka.move(70);
   }
 
 }
